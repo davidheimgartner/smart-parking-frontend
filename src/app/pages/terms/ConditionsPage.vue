1ef84517241f808ec5100bba1f6a0e98d21952f6
hirsch <gery.hirschfeld@w3tec.ch>
2020-07-08 16:44:44 +0200

    feat(terms): add final terms

--- src/app/pages/terms/ConditionsPage.vue
+++ src/app/pages/terms/ConditionsPage.vue
@@ -1,85 +1,86 @@
 <template>
   <section>
-    <Title text="Unsere allgemeinen Geschäftsbedingungen" />
+    <Title text="Allgemeine Geschäftsbedingungen" />
     <Subtitle>1. Zweck/Geltungsbereich</Subtitle>
     <Content>
-      1 Parcandi ist eine Dienstleistung, über welche man Parkplätze der
-      Basler Leben AG (nachfolgend Basler) in den von der Basler bestimmten
-      Gebäuden mieten kann. Die vorliegenden AGB gelten als integrierender
-      Vertragsbestandteil des Mietverhältnisses.
+      Parcandi ist eine Applikation, über welche man Parkplätze der Basler Leben
+      AG oder Basler Versicherung AG (nachfolgend «Basler») in den von der
+      Basler bestimmten Liegenschaften mieten kann. Die vorliegenden allgemeiner
+      Geschäftsbedingungen (nachfolgend AGB) gelten als integrierender
+      Vertragsbestandteil des Mietverhältnisses. Die Nutzung der Anwendung setzt
+      die Zustimmung zu diesen voraus.
     </Content>
     <v-divider></v-divider>
     <Subtitle>2. Allgemeine Bestimmungen</Subtitle>
     <Content>
-      1 In den Gebäuden des vermieteten Parkplatzes gelten, wenn nicht
-      abweichend geregelt, das eidgenössische Strassenverkehrsgesetz (SVG) vom
-      19. Dezember 1958 und die entsprechenden Verordnungen.
+      In den Gebäuden bzw. auf den Grundstücken des vermieteten Parkplatzes
+      gelten, wenn nicht abweichend geregelt, das eidgenössische
+      Strassenverkehrsgesetz (SVG) vom 19. Dezember 1958 und die entsprechenden
+      Verordnungen.
     </Content>
     <v-divider></v-divider>
     <Subtitle>3. Nutzungsvorschriften</Subtitle>
     <Content>
-      1 Fahrzeuge dürfen nur innerhalb der durch den Nutzer reservierten und
-      durch die Basler bestimmten Parkfelder innerhalb der Markierung abgestellt
+      Fahrzeuge dürfen nur innerhalb der durch den Nutzer reservierten und durch
+      die Basler bestimmten Parkfelder innerhalb der Markierung abgestellt
       werden. Nach Ende der Mietzeit sind die Parkfelder unverzüglich
       freizugeben. Widerrechtlich oder verkehrsbehindernd parkierte Fahrzeuge
       können gebührenpflichtig weggeschafft oder blockiert werden.
     </Content>
     <Content>
-      2 Das unnötige Verweilen in den Parkhäusern sowie jede sachfremde
-      Benutzung der Parkfelder und Einrichtungen (z.B. Lagern von Waren,
-      Reparieren oder Waschen von Autos, Verursachen von Verunreinigungen,
-      Befahren mit Spiel- und Sportgeräten) sind untersagt.
+      Das unnötige Verweilen in den Einstellhallen bzw. auf den Grundstücken
+      sowie jede sachfremde Benutzung der Parkfelder und Einrichtungen (z.B.
+      Lagern von Waren, Reparieren oder Waschen von Autos, Verursachen von
+      Verunreinigungen, Befahren mit Spiel- und Sportgeräten) sind untersagt.
     </Content>
     <Content>
-      3 Das Verteilen von Werbematerialien und das Plakatieren sind verboten.
+      Das Verteilen von Werbematerialien und das Plakatieren sind verboten.
     </Content>
-    <Content>4 Das Rauchen in den Parkhäusern ist verboten.</Content>
+    <Content>Das Rauchen in den Einstellhallen ist verboten. </Content>
     <Content>
-      5 Den Weisungen der Basler sowie deren Verwaltung, ist Folge zu leisten.
-    </Content>
-    <Content>
-      6 Durch das Reservieren eines Parkplatzes akzeptiert der Mieter die
-      jeweils für die Einstellhalle anwendbare Hausordnung.
+      Den Weisungen der Basler sowie deren Verwaltung, ist Folge zu leisten.
     </Content>
     <Content>
-      7 Die Basler resp. die Verwaltung der Einstellhalle ist berechtigt, die
-      ihr infolge Missachtung dieser Vorschriften entstehenden Aufwände den
+      Durch das Reservieren eines Parkplatzes akzeptiert der Mieter die jeweils
+      für die Einstellhalle anwendbare Hausordnung.
+    </Content>
+    <Content>
+      Die Basler resp. die Verwaltung der Einstellhalle ist berechtigt, die ihr
+      infolge Missachtung dieser Vorschriften entstehenden Aufwände den
       Verursacherinnen oder den Verursachern zu belasten.
     </Content>
     <v-divider></v-divider>
     <Subtitle>4. Reservieren und Buchen</Subtitle>
     <Content>
-      1 Der Nutzer stellt eine Reservationsanfrage für einen definierten
-      Zeitraum in der gewünschten Einstellhalle. Der Nutzer hat mit der
+      Der Nutzer stellt eine Reservationsanfrage für einen undefinierten
+      Zeitraum für die gewünschte Parkfläche. Der Nutzer hat mit der
       Reservationsanfrage keinen Anspruch auf einen Parkplatz.
     </Content>
     <Content>
-      2 Falls ein Parkplatz zur gewünschten Zeit vorhanden ist, erhält der
-      Nutzer eine Reservationsbestätigung für einen bestimmten Parkplatz für den
-      gewählten Zeitraum. Wenn die Reservationsbestätigung auf einen festen
+      Falls ein Parkplatz zur aktuellen Zeit vorhanden ist, erhält der Nutzer
+      eine Reservationsbestätigung für einen bestimmten Parkplatz für den
+      benötigten Zeitraum. Wenn die Reservationsbestätigung auf einen festen
       Parkplatz mit Parkplatznummer verweist, ist diese bindend. Ein Parkieren
       ausserhalb dieses Parkplatzes ist nicht gestattet.
     </Content>
     <Content>
-      3 Die Basler übernimmt keinerlei Haftung für die Verfügbarkeit des
+      Die Basler übernimmt keinerlei Haftung für die Verfügbarkeit des
       Parkplatzes und allfällige damit verbundene Folgekosten. Dies gilt
       insbesondere, wenn dieser widerrechtlich belegt ist. Bei Verfügbarkeit und
       nach Rücksprache mit der Basler kann der Nutzer einen Ausweichparkplatz
       reservieren.
     </Content>
+    <Content> Unfälle mit Personenschäden sind der Polizei zu melden. </Content>
     <Content>
-      4 Unfälle mit Personenschäden sind der Polizei zu melden.
-    </Content>
-    <Content>
-      5 Beschädigungen und Defekte in den Einstellhallen und ihrer Einrichtungen
-      (Schranken, Kassen, Lifte, Beleuchtung, Signale usw.) sind der Basler,
-      resp. der zuständigen Verwaltung zu melden.
+      Beschädigungen und Defekte in den Einstellhallen oder auf den Grundstücken
+      und ihrer Einrichtungen (Schranken, Kassen, Lifte, Beleuchtung, Signale
+      usw.) sind der Basler, resp. der zuständigen Verwaltung zu melden.
     </Content>
     <v-divider></v-divider>
     <Subtitle>5. Tarife</Subtitle>
     <Content>
-      1 Die Gebühren werden durch Parcandi erhoben und richten sich nach
-      der ununterbrochenen Mietzeit.
+      Die Gebühren werden durch die Basler erhoben und richten sich nach der
+      ununterbrochenen Mietzeit.
     </Content>
     <ul>
       <li>Der Basistarif beträgt CHF 2. -pro angefangene Stunde.</li>
@@ -89,28 +90,152 @@
       </li>
     </ul>
     <Content>
-      2 In den Gebühren ist die gesetzlich geschuldete Mehrwertsteuer enthalten.
+      In den Gebühren ist die gesetzlich geschuldete Mehrwertsteuer enthalten.
     </Content>
     <v-divider></v-divider>
-    <Subtitle>6. Haftungsbestimmungen</Subtitle>
+    <Subtitle>6. Haftungsbestimmungen für Parkplatznutzung </Subtitle>
     <Content>
-      1 Die Benutzung der Parkhäuser erfolgt auf eigene Verantwortung und
-      eigenes Risiko. Fahrzeuge und Personen werden nicht bewacht.
+      Die Benutzung der Parkplätze erfolgt auf eigene Verantwortung und eigenes
+      Risiko. Fahrzeuge und Personen werden nicht bewacht.
     </Content>
     <Content>
-      2 Für Unfälle sowie Personen- und Sachschäden wird jede Haftung der Basler
+      Für Unfälle sowie Personen- und Sachschäden wird jede Haftung der Basler
       abgelehnt.
     </Content>
     <Content>
-      3 Für Diebstähle und Sachbeschädigungen durch Dritte und für Schäden
-      infolge von äusseren Einflüssen (Feuer, Explosion, Wasser, Eis, Schnee
-      usw.) wird jede Haftung der Basler abgelehnt.
+      Für Diebstähle und Sachbeschädigungen durch Dritte und für Schäden infolge
+      von äusseren Einflüssen (Feuer, Explosion, Wasser, Eis, Schnee usw.) wird
+      jede Haftung der Basler abgelehnt.
     </Content>
     <v-divider></v-divider>
-    <Subtitle>7. Gerichtsstand</Subtitle>
+    <Subtitle>7. Haftungsbestimmungen für Nutzung der App </Subtitle>
     <Content>
-      Der Gerichtsstand für sämtliche Rechtsstreitigen aus dem Mietverhältnis
+      Die Installation und Nutzung der Applikation erfolgt auf eigenes Risiko.
+      Die Basler übernimmt keine Gewährleistung für die Anwendung und leistet
+      keinen Support. Die Basler garantiert nicht, dass die Anwendung und die
+      Dienstleistungen jederzeit fehler- und unterbruchfrei funktionieren bzw.
+      zur Verfügung stehen, oder dass Fehler behoben werden.
+    </Content>
+    <Content
+      >Die Basler lehnt, soweit gesetzlich zulässig, jegliche Haftung für die
+      Richtigkeit, Angemessenheit und Aktualität der über die Anwendung
+      gelieferten oder der auf Webseiten der Basler bzw. Dritter zur Verfügung
+      gestellten Informationen ab. Insbesondere übernimmt die Basler keinerlei
+      Verantwortung oder Haftung für Handlungen, welche aufgrund dieser
+      Information erfolgen oder unterbleiben.
+    </Content>
+    <br />
+    <Content
+      >Die Anwendung kann Hyperlinks zu Webseiten Dritter (nachfolgend:
+      „Drittwebseiten“) enthalten, welche nicht von der Basler betrieben werden.
+      Das Aufrufen von solchen Hyperlinks erfolgt auf eigene Gefahr. Der Nutzer
+      akzeptiert, dass die Basler nicht für die Überprüfung oder Bewertung der
+      Inhalte auf diesen Drittwebseiten zuständig und verantwortlich ist. Die
+      Inhalte der Drittwebseiten widerspiegeln nicht zwingend die Meinung der
+      Basler. Die Basler übernimmt keine Haftung für die Inhalte solcher
+      Drittwebseiten.
+    </Content>
+    <Content
+      >Die Basler lehnt, soweit gesetzlich zulässig, jegliche Haftung für
+      inhärente Risiken auf dem Internet ab. Darunter fallen insbesondere:
+    </Content>
+    <ul>
+      <li>
+        <Content
+          >technische oder organisatorische Schwierigkeiten beim Empfang bzw.
+          der Übermittlung der Daten
+        </Content>
+      </li>
+      <li>
+        <Content
+          >technische Mängel, Störungen oder Unterbrechungen des Telefonnetzes
+          bzw. des Internetzugangs
+        </Content>
+      </li>
+      <li>
+        <Content
+          >rechtswidrige Eingriffe Dritter in Hardware, Software oder
+          Netzwerke</Content
+        >
+      </li>
+      <li><Content>Platzieren von Viren </Content></li>
+      <li>
+        <Content>Kopieren und Verfälschen von Daten und Informationen</Content>
+      </li>
+      <li><Content>Überlastung der Netze</Content></li>
+      <li>
+        <Content
+          >durch Dritte verursachte mutwillige oder zufällige Blockierung
+          elektronischer Zugänge</Content
+        >
+      </li>
+    </ul>
+    <Content
+      >Die Basler lehnt, soweit gesetzlich zulässig, jegliche Haftung für
+      Schäden, die mit dem Zugriff auf die Anwendung aus dem Ausland
+      zusammenhängen ab.
+    </Content>
+    <br />
+    <Content
+      >Der Nutzer haftet der Basler vollumfänglich und unbeschränkt für
+      sämtliche Verstösse gegen diese AGB. Der Nutzer verpflichtet sich, die
+      Basler für sämtliche gegen sie erhobene Forderungen, welche im
+      Zusammenhang mit Verstössen gegen die vorliegenden AGB stehen, schadlos zu
+      halten.
+    </Content>
+
+    <Subtitle>8. Nutzungs- und Eigentumsrechte </Subtitle>
+    <Content
+      >Die Basler erteilt dem Nutzer das eingeschränkte, unübertragbare, nicht
+      exklusive Recht zur Nutzung der Applikation und der darin angebotenen
+      Leistungen. Die Eigentums- und Urheberrechte an sämtlichen Inhalten
+      verbleiben vollständig bei der Basler. Alle daraus fliessenden Rechte
+      betreffend Vervielfältigung, Vermarktung und Verbreitung (inkl.
+      Vermietung, Verleihung, Leasing und Lizenzierung) sowie auch das Eigentum
+      an der Anwendung verbleiben ausschliesslich bei der Basler.
+    </Content>
+
+    <Subtitle>9. Abtretung von Nutzerrechten </Subtitle>
+    <Content
+      >Mit der Benutzung der Anwendung gewährt der Nutzer der Basler das
+      kostenlose, unwiderrufliche, unbeschränkte, unlimitierte und nicht
+      exklusive Recht, alle Bemerkungen, Verbesserungsvorschläge, Kommentare,
+      Ideen, Bilder und Grafiken sowie sämtliche andere durch diese Anwendung an
+      die Basler übertragenen Informationen zu verwenden, zu reproduzieren, zu
+      verändern, anzupassen, zu publizieren, zu übersetzen, zu verbreiten und
+      anzuzeigen.
+    </Content>
+
+    <Subtitle>10. Änderungen </Subtitle>
+    <Content
+      >Diese AGB können durch die Basler jederzeit nach eigenem Ermessen
+      geändert werden. Durch die Weiterbenutzung der Anwendung nach einer
+      Änderung der AGB akzeptiert der Nutzer die Änderungen vollumfänglich und
+      vorbehaltlos.
+    </Content>
+    <Content
+      >Die Basler verpflichtet sich, auf Nutzungsänderungen hinzuweisen.
+    </Content>
+
+    <Subtitle>11. Massgebende Version </Subtitle>
+    <Content
+      >Bei Unklarheiten in den verschiedenen Sprachversionen der AGB ist die
+      deutsche Version massgebend.
+    </Content>
+
+    <Subtitle>12. Gerichtstand </Subtitle>
+    <Content
+      >Der Gerichtstand für sämtliche Rechtsstreitigen aus dem Mietverhältnis
       ist Basel.
+    </Content>
+
+    <Subtitle>13. Salvatorische Klausel</Subtitle>
+    <Content
+      >Sollte eine der obgenannten Bestimmungen gegen geltendes Recht
+      verstossen, hindert dies die Gültigkeit der restlichen Bestimmungen nicht.
+      Die ungültige Bestimmung ist in diesem Falle mit einer rechtskonformen
+      Regelung zu ersetzen, welche der ursprünglichen Bestimmung dem Sinn nach
+      am nächsten kommt.
     </Content>
   </section>
 </template>
--- src/app/pages/terms/DataPrivacyPage.vue
+++ src/app/pages/terms/DataPrivacyPage.vue
@@ -1,11 +1,288 @@
 <template>
   <section>
-    <Title text="Datenschutz" />
+    <Title text="Informationen zur Datenverarbeitung Parcandi" />
+    <Subtitle>1. Verantwortliche Stelle und Kontakt </Subtitle>
     <Content>
-      Mit der Nutzung der Dienstleistung der Basler akzeptiert der Nutzer die
-      Datenschutzbestimmungen für Mietverhältnisse
-      https://www.baloise.ch/de/ueber-uns/datenverarbeitung-mietverhaeltnisse.html
-      und bestätigt diese gelesen zu haben.
+      Die vorliegenden Datenschutzinformationen gelten für die Nutzung der
+      Services der Parcandi Applikation (nachfolgend «App»). Mit den
+      vorliegenden Informationen möchten wir Ihnen darlegen, wann wir im Rahmen
+      der Nutzung der Parcandi Services welche Daten erheben, wie wir Ihre Daten
+      verarbeiten und welche Recht Sie im Zusammenhang mit der Verarbeitung
+      Ihrer Daten durch uns haben.
+    </Content>
+    <Content>
+      Verantwortliche Stelle für die hier beschriebenen Datenverarbeitungen ist:
+    </Content>
+    <Content>
+      Basler Leben AG<br />
+      Aeschengraben 21<br />
+      4002 Basel
+    </Content>
+    <Content>(nachfolgend «Basler» oder «wir» genannt). </Content>
+
+    <v-divider></v-divider>
+
+    <Subtitle>2. Erhebung von personenbezogenen Daten </Subtitle>
+    <Content
+      >Wir verarbeiten u.a. folgende von Ihnen zur Verfügung gestellten oder von
+      Dritten eingeholten personenbezogenen Daten (nachfolgend «Daten»):
+    </Content>
+    <ul>
+      <li>
+        <Content
+          >Um die Services über die App nutzen zu können, erheben wir das
+          Kennzeichen Ihres Fahrzeuges sowie Ihre Handynummer.
+        </Content>
+      </li>
+      <li>
+        <Content
+          >Die Registrierung von Zahlungsmitteln erfolgt ohne Mitwirkung der
+          Basler bei unserem ECommerce Vertragspartner «Datatrans». Nachdem Sie
+          die Bezahlung ausgeführt haben, erhalten wir eine Referenznummer, um
+          die Zahlung Ihrer Buchung zuzuordnen.
+        </Content>
+      </li>
+      <li>
+        <Content
+          >Die App sammelt bei der Benutzung automatisch technische Daten über
+          das Betriebssystem, zu den Spezifikationen des Endgerätes, zum
+          Nutzungsverhalten sowie den Nutzungszeiten.
+        </Content>
+      </li>
+      <li>
+        <Content
+          >Falls Sie mit uns in Kontakt treten (innerhalb oder ausserhalb der
+          App), erheben wir Ihre mit dieser Korrespondenz übermittelten Daten
+          (z.B. Kontaktangaben wie E-Mail-Adresse oder Vorund Nachname).
+        </Content>
+      </li>
+      <li>
+        <Content
+          >Im Falle eines Missbrauchs können wir im Einzelfall weitere Daten
+          (Kontaktdaten des Halters des Fahrzeuges) erheben.
+        </Content>
+      </li>
+    </ul>
+
+    <v-divider></v-divider>
+
+    <Subtitle>3. Zwecke der Datenverarbeitung </Subtitle>
+    <Content
+      >Ihre Daten werden von uns für diejenigen Zwecke verwendet, welche wir
+      Ihnen bei deren Erhebung aufgezeigt haben, oder wenn wir dazu gesetzlich
+      verpflichtet sind.
+    </Content>
+    <Content>Die erhobenen Daten bearbeiten wir wie folgt: </Content>
+    <ul>
+      <li>
+        <Content
+          >Ihre Daten erheben, speichern und bearbeiten wir, soweit dies für die
+          Abwicklung des Vertrages, die Verwaltung der Kundenbeziehung und die
+          Beantwortung von Fragen und Anliegen notwendig ist.
+        </Content>
+      </li>
+      <li>
+        <Content
+          >Unser Partner «Twilio» verifiziert bei der Transaktion Ihre
+          hinterlegte Handynummer und sendet Ihnen einen Code per SMS, um die
+          Buchung zu bestätigen.
+        </Content>
+      </li>
+      <li>
+        <Content
+          >Ihre Daten verwenden wir zudem, um mit Ihnen bei Rückfragen Kontakt
+          aufzunehmen.
+        </Content>
+      </li>
+      <li>
+        <Content
+          >Wir können Ihre Daten für periodische interne Auswertungen in
+          anonymisierter Form zu Statistikzwecken verwenden.
+        </Content>
+      </li>
+      <li>
+        <Content
+          >Um die Benutzerfreundlichkeit der App laufend zu erhöhen, bearbeiten
+          wir die erhobenen Angaben zum Nutzungsverhalten, um systembedingte
+          Anpassungen zu machen.
+        </Content>
+      </li>
+      <li>
+        <Content
+          >Die Daten können wir zudem verwenden, um Sie von Zeit zu Zeit auf
+          Angebote von uns aufmerksam zu machen oder Sie für Benutzerumfragen
+          oder Kundenaktionen zu kontaktieren. Sie haben jederzeit die
+          Möglichkeit, uns mitzuteilen, wenn Sie keine diesbezügliche
+          Kontaktaufnahme wünschen.
+        </Content>
+      </li>
+    </ul>
+
+    <Subtitle
+      >4. Auftragsdatenverarbeiter und Datenaustausch mit Dritten
+    </Subtitle>
+    <Content
+      >Um Ihnen eine effiziente und optimal funktionierende App zur Verfügung zu
+      stellen, lassen wir einen Teil unserer Leistungen z.B. Bereinigen von
+      Zustellungsfehlern, Löschen sowie Speichern der Daten in unserem Auftrag
+      durch rechtliche selbstständige Unternehmen im In- und Ausland erbringen.
+      Diese Dienstleister (Auftragsdatenverarbeiter) werden vertraglich
+      verpflichtet, sich ebenfalls an unsere festgelegten Zwecke der
+      Datenverarbeitung sowie die anwendbaren gesetzlichen
+      Datenschutzbestimmungen zu halten. Befindet sich ein Dienstleister in
+      einem Staat, dessen Datenschutzgesetzgebung von den zuständigen
+      Aufsichtsbehörden als nicht angemessen taxiert wird, verpflichten wir
+      diesen vor einer Übermittlung Ihrer Daten mit den von der europäischen
+      Kommission eingeführten und vom Eidgenössischen Datenschutz- und
+      Öffentlichkeitsbeauftragten (EDÖB) anerkannten EU-Standardvertragsklauseln
+      oder gleichwertiger Garantien einen angemessenen Schutz zu garantieren.
+    </Content>
+    <Content
+      >Die Basler verwendet die Entwicklerplattform «Google Firebase» und die
+      mit ihr verbundenen Funktionen und Dienste, angeboten von Google LLC 1600
+      Amphitheatre Pkwy Mountain View, CA, 94043 USA. Bei Google Firebase
+      handelt es sich um eine Plattform für Entwickler von Applikationen für
+      mobile Geräte und Webseiten (<a
+        href="https://firebase.google.com/products/"
+        target="_blank"
+        >https://firebase.google.com/products/</a
+      >). Die vorliegende App verwendet folgende Dienste von «Google Firebase»:
+    </Content>
+    <ul>
+      <li>
+        <Content
+          >«Firebase Analytics» für die Auswertung der Nutzung und Interaktion
+          mit der App. Dabei erfasst werden Ereignisse, wie erstmaliges Öffnen
+          der App, Deinstallation, Updates, Absturz oder Häufigkeit der Nutzung
+          der App.
+        </Content>
+      </li>
+      <li>
+        <Content>«Firebase Database» zum Speichern der Buchungsdaten </Content>
+      </li>
+    </ul>
+
+    <Content
+      >Die Datenschutzerklärung von Google ist unter
+      <a href="https://google.com/policies/privacy" target="_blank"></a>
+      abrufbar. Google ist unter dem Privacy-Shield-Abkommen zertifiziert und
+      bietet hierdurch die nötige Garantie, ein zum schweizerischen und
+      europäischen Datenschutzrecht angemessenen Schutz zu
+      gewährleisten.</Content
+    >
+    <Content
+      >Für die Behebung von Problemen im Zusammenhang mit den vermieteten
+      Parkfeldern und im Falle eines Missbrauchs können wir Ihre Daten an andere
+      zur Baloise-Gruppe gehörende Gesellschaften (insbesondere die jeweilige
+      Eigentümerin oder Verwalterin einer Immobilie) weitergeben.
+    </Content>
+    <Content
+      >Ihre Daten können zu den vorerwähnten Zwecken zudem weiteren Empfängern
+      (z.B. Gutachtern bei einem Schadenfall, Beteiligten in Rechtsverfahren
+      oder Erwerber von Geschäftsbereichen der Basler) weitergegeben werden.
+    </Content>
+    <Content
+      >Bei Anfragen staatlicher Behörden (z.B. bei einem Strafverfahren) werden
+      wir die erforderlichen Daten bekannt geben, sofern und soweit dies
+      gesetzlich vorgeschrieben und wir dazu verpflichtet sind.
+    </Content>
+
+    <v-divider></v-divider>
+
+    <Subtitle>5. Datensicherheit </Subtitle>
+    <Content
+      >Wie behandeln Ihre Daten vertraulich und halten uns an die anwendbaren
+      Datenschutzbestimmungen. Zudem orientieren wir uns an anerkannten
+      Sicherheitsstandards wie z.B. ISO 27001.
+    </Content>
+    <Content
+      >Wir treffen geeignete technische und organisatorische Massnahmen, um Ihre
+      Daten gegen vorhersehbare Risiken zu schützen. Ihre über unsere App
+      übermittelten Daten schützen wir auf dem Transportweg durch geeignete
+      Verschlüsselungsmechanismen.
+    </Content>
+    <Content
+      >Ihr Endgerät befindet sich dabei aber ausserhalb des von uns
+      kontrollierbaren Sicherheitsbereichs. Sie sind daher angehalten, sich über
+      erforderliche Sicherheitsvorkehrungen zu informieren und diesbezüglich
+      geeignete Massnahmen treffen. Die Basler haftet keinesfalls für Schäden,
+      die Ihnen aus Datenverlust oder -manipulation entstehen können.
+    </Content>
+    <Content
+      >Wir behalten uns ausdrücklich das Recht vor, bei Feststellung von
+      Sicherheitsrisiken den Zugang zu unser App bis zu deren Behebung zu
+      unterbrechen bzw. in gravierenden Fällen zu sperren. Wir schliessen jede
+      Haftung für sämtliche infolge der Unterbrechung oder der Sperre
+      entstandener Schäden oder Folgeschäden aus.
+    </Content>
+
+    <v-divider></v-divider>
+
+    <Subtitle>6. Ihre Rechte in Bezug auf Ihre Daten </Subtitle>
+    <Content
+      >Wollen Sie Auskunft über Ihre von uns verarbeiteten Daten haben, können
+      Sie uns Ihr Auskunftsgesuch schriftlich mit Angabe Ihres Kennzeichens und
+      Ihrer Handynummer, unter Beilage einer Kopie Ihrer Identitätskarte oder
+      Ihres Passes sowie der Quittung eines Parkvorganges zustellen.
+    </Content>
+    <Content
+      >Sie können die Berichtigung unrichtiger Daten, die Ergänzung
+      unvollständiger Daten, die Einschränkung der Verarbeitung Ihrer Daten und
+      in bestimmten Fällen die Übertragung Ihrer Daten von uns verlangen. Sie
+      können auch die Löschung Ihrer Daten verlangen, soweit wir nicht wegen
+      geltender Gesetze und Vorschriften zur Aufbewahrung Ihrer Daten
+      verpflichtet oder ermächtigt sind.
+    </Content>
+    <Content
+      >Sie können die Nutzung der App jederzeit beenden und die App vom Endgerät
+      entfernen.
+    </Content>
+    <Content
+      >Wünschen Sie nicht beworben zu werden, können Sie dies uns ebenfalls
+      jederzeit schriftlich mitteilen.
+    </Content>
+    <Content
+      >Ihre Gesuche können Sie per Mail an
+      <a href="mailto:parcandi@baloise.ch">parcandi@baloise.ch</a> oder an die
+      unter Ziffer 9 aufgeführte Adresse senden.
+    </Content>
+
+    <v-divider></v-divider>
+
+    <Subtitle>7. Speicherungsdauer </Subtitle>
+    <Content
+      >Ihre Daten werden im Einklang mit unseren Löschkonzepten nur so lange
+      gespeichert, wie es für die Erreichung der vorgenannten Zwecke
+      erforderlich ist oder wie wir ermächtigt bzw. gesetzlich verpflichtet
+      sind, diese länger aufzubewahren.
+    </Content>
+
+    <v-divider></v-divider>
+
+    <Subtitle>8. Änderungen dieser Information zur Datenverarbeitung </Subtitle>
+    <Content
+      >Wir behalten uns das Recht vor, die vorliegende Information zur
+      Datenverarbeitung jederzeit abzuändern resp. zu ergänzen. Bei jeder
+      Benutzung der App wird Ihnen die aktuelle Version beim Abschluss der
+      Buchung zur Kenntnis gebracht.
+    </Content>
+
+    <v-divider></v-divider>
+
+    <Subtitle>9. Kontaktstelle für Fragen zum Datenschutz </Subtitle>
+    <Content
+      >Wenn Sie Fragen zu den Erläuterungen zum Datenschutz haben, können Sie
+      sich an den Datenschutzbeauftragten der Basler wenden, der auch im Falle
+      von Auskunftsersuchen, Anregungen oder Beschwerden zur Verfügung steht.
+    </Content>
+    <Content>
+      Basler Leben AG <br />
+      Datenschutzbeauftragter<br />
+      Aeschengraben 21, Postfach<br />
+      CH-4002 Basel<br /><br />
+      <a href="mailto:datenschutz@baloise.ch">datenschutz@baloise.ch</a>
+      <br /><br />
+      Basel, 03. Juli 2020
     </Content>
   </section>
 </template>
@@ -13,11 +290,13 @@
 <script lang="ts">
 import { defineComponent } from "@vue/composition-api"
 import Title from "@/app/components/common/Title.vue"
+import Subtitle from "@/app/components/common/Subtitle.vue"
 import Content from "@/app/components/common/Content.vue"
 
 export default defineComponent({
   components: {
     Title,
+    Subtitle,
     Content,
   },
 })
